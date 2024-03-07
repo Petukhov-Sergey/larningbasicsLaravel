59776ef9cc36c49e68ab68d17534883d01a48eec
Petukhov-Sergey <9499goginsan9499@gmail.com>
2024-03-07 12:18:47 +0300

    last commit

---
+++ app/Http/Filters/PostFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Filtres;
+
+use App\Http\Filters\AbstractFilter;
+use Illuminate\Database\Eloquent\Builder;
+
+class PostFilter extends AbstractFilter
+{
+    public const TITLE = 'title';
+    public const BODY = 'body';
+    public const CATEGORY_ID = 'category_id';
+
+    protected function getCallbacks(): array
+    {
+        return [
+            self::TITLE=>[$this, 'title'],
+            self::BODY=>[$this, 'body'],
+            self::CATEGORY_ID=>[$this, 'categoryId'],
+        ];
+    }
+
+    public function title(Builder $builder, $value)
+    {
+        $builder->where('title', 'like', "%{$value}%");
+    }
+
+    public function body(Builder $builder, $value)
+    {
+        $builder->where('body', 'like', "%{$value}%");
+    }
+    public function categoryId(Builder $builder, $value)
+    {
+        $builder->where('category_id', $value);
+    }
+}
